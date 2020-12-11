ddf8cb4bf7c1ce94ba0e22ff3f0b3e8ae3c4e11d
Caseykins <caseyjenkins92@gmail.com>
2020-12-11 16:00:18 -0700

    updates app.use routes

--- server.js
+++ server.js
@@ -35,6 +35,7 @@ app.get('/profile', (req, res) => {
 });
 
 app.use('/auth', require('./routes/auth'));
+app.use('/board', isLoggedIn, require('./routes/board'));
 
 var server = app.listen(process.env.PORT || 3000, ()=> console.log(`🎧You're listening to the smooth sounds of port ${process.env.PORT || 3000}🎧`));
 
