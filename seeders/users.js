f0afa7bc152d5234531d7442df3a7412f2539db5
Caseykins <caseyjenkins92@gmail.com>
2020-12-10 19:14:34 -0700

    sets up models in database

---
+++ seeders/users.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const bulkUsers = await queryInterface.bulkInsert('users', [
+      {
+        email: 'casey@test.com',
+        name: 'casey',
+        userName: 'Caseykins',
+        password: 'password',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        email: 'bob@test.com',
+        name: 'bob',
+        userName: 'Bobby',
+        password: 'password',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        email: 'jimbo@test.com',
+        name: 'jim',
+        userName: 'Jimboy',
+        password: 'password',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        email: 'billy@test.com',
+        name: 'billy',
+        userName: 'Big Bill',
+        password: 'password',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {returning: true})
+    console.log("bulkInsert: ", bulkUsers)
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
