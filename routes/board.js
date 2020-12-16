ed79fb8a1e0dfbad87f6b64031fa319f53570c3f
Caseykins <caseyjenkins92@gmail.com>
2020-12-15 18:17:16 -0700

    still working through task routes/ejs

--- routes/board.js
+++ routes/board.js
@@ -11,12 +11,12 @@ router.use(methodOverride("_method"));
 
 router.get("/board", isLoggedIn, (req, res) => {
     let userName = req.user.userName
-    db.cardLists.findAll({
+    db.lists.findAll({
         where: {
             userId: req.user.id,
-        }
-    }).then( function(foundList) {
-        res.render('board', { userName: userName, foundList: foundList } )
+        }, include: [db.tasks]
+    }).then( function(foundList, foundTask) {
+        res.render('board', { userName: userName, foundList: foundList, foundTask: foundTask } )
     })
 })
 
@@ -24,7 +24,7 @@ router.get("/board", isLoggedIn, (req, res) => {
 
 // Create New List
 router.post('/board', (req, res) => {
-    db.cardLists.create({
+    db.lists.create({
         listName: req.body.title,
         userId: req.user.id 
     }).then( function (createdList) { 
@@ -34,7 +34,7 @@ router.post('/board', (req, res) => {
 
 // Update List Title
 router.put('/board/:id', isLoggedIn, (req, res) => {
-    db.cardLists.update({
+    db.lists.update({
         listName: req.body.newTitle,
     }, {
         where: {
@@ -47,7 +47,7 @@ router.put('/board/:id', isLoggedIn, (req, res) => {
 
 // Delete List
 router.delete('/board/:id', isLoggedIn, (req, res) => {
-    db.cardLists.destroy({
+    db.lists.destroy({
         where: {
             id: req.params.id,
         }
@@ -58,20 +58,18 @@ router.delete('/board/:id', isLoggedIn, (req, res) => {
 
 // ============== Tasks ===================
 
-
 // Add a new task to list
 router.post('/board/:id', (req, res) => {
-    console.log(req.body.addTask, "========add task=======")
-    console.log(req.user.id, "======user id======")
-    console.log(req.params.id, "======params id(list id)====")
-    console.log(req.user.userName, "======User Name====")
-    db.cards.create({
-        cardName: req.user.userName + "New Task",
-        description: req.body.addTask,
+    // console.log(req.body.addTask, "========add task=======")
+    // console.log(req.user.id, "======user id======")
+    // console.log(req.params.id, "======params id(list id)====")
+    // console.log(req.user.userName, "======User Name====")
+    db.tasks.create({
+        name: req.body.addTask,
         userId: req.user.id,
-        cardList: req.params.id
-    }).then( function (foundTask) { 
-        res.redirect('/board', { foundTask: foundTask })
+        listId: req.params.id
+    }).then( function () { 
+        res.redirect('/board')
     })
 })
 
@@ -79,30 +77,30 @@ router.post('/board/:id', (req, res) => {
 
 // strike() goes here....?
 
-// router.put('/board/:id', isLoggedIn, (req, res) => {
-//     // console.log(req.user.id, '=========user id=========')
-//     // console.log(req.params.id, '=========params id=======')
-//     // console.log(req.body, '=========req body=======')
-//     db.cards.update({
-//         cardName: req.body.newTitle,
-//     }, {
-//         where: {
-//             id: req.params.id
-//         }
-//     }).then( function(changeTitle) {
-//         res.redirect('/board')
-//     })
-// })
+router.put('/board', isLoggedIn, (req, res) => {
+    // console.log(req.user.id, '=========user id=========')
+    console.log(req.params.id, '=========params id=======')
+    console.log(req.body, '=========req body=======')
+    db.tasks.update({
+        name: req.body.addTask,
+    }, {
+        where: {
+            id: req.params.id
+        }
+    }).then( function(editTask) {
+        res.redirect('/board')
+    })
+})
 
 // Delete Task (might forego this and only use edit)
-// router.delete('/board/:id', isLoggedIn, (req, res) => {
-//     db.cardLists.destroy({
-//         where: {
-//             id: req.params.id,
-//         }
-//     }).then((removedList) => {
-//         res.redirect('/board') 
-//     })
-// })
+router.delete('/board/:id', isLoggedIn, (req, res) => {
+    db.lists.destroy({
+        where: {
+            id: req.params.id,
+        }
+    }).then((removedList) => {
+        res.redirect('/board') 
+    })
+})
 
 module.exports = router;
